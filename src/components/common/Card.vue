c7b6d52076ae1c227cd648d9b8e2d6f0a07c6917
rmingon <rmingon@doing.fr>
2023-02-13 16:26:02 +0100

    add card

---
+++ src/components/common/Card.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+    <slot></slot>
+  </a>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
