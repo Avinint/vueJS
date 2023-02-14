403ac1c47fc5f70ceec469ef9a4427bc8bdf1e51
rmingon <rmingon@doing.fr>
2023-02-14 15:05:57 +0100

    fix card

--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -1,7 +1,7 @@
 <template>
-  <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+  <div class="p-4 bg-white border border-gray-200 rounded-lg shadow">
     <slot></slot>
-  </a>
+  </div>
 </template>
 
 <script setup>
