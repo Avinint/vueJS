2d3bf804d8f151d58bc12c8237bcfd81927edfae
rmingon <rmingon@doing.fr>
2023-02-14 15:06:24 +0100

    add store in login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -7,10 +7,7 @@
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <!--<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Connecter vous à votre compte
-          </h1>-->
-          <form class="space-y-4 md:space-y-6" @submit.prevent="user.login(mail, password)">
+          <form class="space-y-4 md:space-y-6" @submit.prevent="login()">
             <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre E-mail</label>
               <input v-model="mail" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
@@ -18,16 +15,6 @@
             <div>
               <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
               <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-            </div>
-            <div class="flex items-center justify-between">
-              <!--<div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                </div>
-                <div class="ml-3 text-sm">
-                  <label for="remember" class="text-gray-500 dark:text-gray-300"></label>
-                </div>
-              </div>-->
             </div>
             <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Se connecter</button>
           </form>
@@ -49,5 +36,14 @@
   const password = ref('')
 
   const user = useUserStore()
+
+  const login = () => {
+    try {
+      user.login(mail.value, password.value)
+      router.push('/')
+    } catch (e) {
+      alert(JSON.stringify(e))
+    }
+  }
 
 </script>
