4f15673ddd765207f2a0e6ee8036745011e51d2f
rmingon <rmingon@doing.fr>
2023-02-14 17:18:14 +0100

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
 const defaultHeaders = {
-  'Content-Type': 'application/ld+json',
   'Accept': 'application/json',
   // 'Authorization': 'Bearer '+ localStorage.getItem('token')
 }
 
-export const getClients = async () => {
+export const getClients = async (page = 1, query = '') => {
   console.log(localStorage.getItem('token'))
-  const response = await fetch(import.meta.env.VITE_API_URL +'/api/clients?page=1', {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/clients?page=${page}${query}`, {
     method:'get',
     headers: {
       ...defaultHeaders,
+      'Content-Type': 'application/ld+json',
       'Authorization': 'Bearer '+ localStorage.getItem('token')
     }
   })
@@ -23,21 +23,42 @@ export const postClient = async (client) => {
     method:'post',
     headers: {
       ...defaultHeaders,
+      'Content-Type': 'application/ld+json',
       'Authorization': 'Bearer '+ localStorage.getItem('token')
     },
     body: JSON.stringify(client)
   })
+  if (response.status !== 201)
+    throw response
   return response.json()
 }
 
 export const updateClient = async (client, id) => {
   const response = await fetch(`${import.meta.env.VITE_API_URL}/api/clients/${id}`, {
-    method:'patch',
+    method:'put',
     headers: {
       ...defaultHeaders,
+      // 'Content-Type': 'application/merge-patch+json',
+      'Content-Type': 'application/ld+json',
       'Authorization': 'Bearer '+ localStorage.getItem('token')
     },
     body: JSON.stringify(client)
   })
+  if (response.status !== 200)
+    throw response
+  return response.json()
+}
+
+export const deleteClient = async (id) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/clients/${id}`, {
+    method:'delete',
+    headers: {
+      ...defaultHeaders,
+      'Content-Type': 'application/merge-patch+json',
+      'Authorization': 'Bearer '+ localStorage.getItem('token')
+    }
+  })
+  if (response.status !== 204)
+    throw response
   return response.json()
 }
