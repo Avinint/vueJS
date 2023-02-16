a13351e445039af9c4da24f5f988f729f3316b13
rmingon <rmingon@doing.fr>
2023-02-16 10:28:54 +0100

    add generic input in login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -8,8 +8,8 @@
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <form class="space-y-4 md:space-y-6" @submit.prevent="login()">
-            <Input v-model="mail" label="Votre E-mail" placeholder="name@company.com" type="email" :id="'email'" :inline="false"/>
-            <Input v-model="password" label="Mot de passe" placeholder="••••••••" type="password" :id="'password'" test="password" :inline="false"/>
+            <Input v-model="mail" label="Votre E-mail" placeholder="name@company.com" type="email" :id="'email'" :inline="false" :test="'email'"/>
+            <Input v-model="password" label="Mot de passe" placeholder="••••••••" type="password" :id="'password'" :test="'password'" :inline="false"/>
             <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Se connecter</button>
           </form>
         </div>
