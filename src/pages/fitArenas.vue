b23e9af38728171d03c3d8a3bfdc2c7f1fd79125
Loic Bertholet <lbertholet@doing.fr>
2023-03-02 16:27:02 +0100

    mise a jour

--- src/pages/fitArenas.vue
+++ src/pages/fitArenas.vue
@@ -81,7 +81,7 @@
       </div>
       <div class="flex items-center">
         <label class="block mb-2 text-sm font-medium text-gray-900 w-1/2">Commentaire</label>
-        <input :readonly="readonly" v-model="commentaire" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
+        <textarea :readonly="readonly" v-model="commentaire" type="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
       </div>
       <div class="flex items-center">
         <span class="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">Actif : </span>
