5e186a2a694e02ee6be9bc56055cf26c07a65345
rmingon <rmingon@doing.fr>
2023-04-03 15:53:03 +0200

    remove log

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,8 +4,6 @@
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
         <img class="w-8 h-8 mr-2" src="../assets/logo.png" alt="logo">
         FitArena
-        {{ valid }}
-        {{ validation }}
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
