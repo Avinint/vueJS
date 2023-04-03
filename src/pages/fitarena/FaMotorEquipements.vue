65cefb93da03b92fbbf3ae8cd0feda068a47d4c0
Loic Bertholet <lbertholet@doing.fr>
2023-04-03 17:23:23 +0200

    mise a jour statut des element motorise

--- src/pages/fitarena/FaMotorEquipements.vue
+++ src/pages/fitarena/FaMotorEquipements.vue
@@ -30,7 +30,7 @@
               </td>
               <td class="px-6 py-4">
                 <label class="relative inline-flex items-center cursor-pointer">
-                  <input type="checkbox" value="true" class="sr-only peer" v-if="equipementTemp.statut"  @change="modifieEquipement(equipementTemp)" >
+                  <input type="checkbox" value="true" class="sr-only peer" v-model="equipementTemp.statut"  @change="modifieEquipement(equipementTemp)" >
                   <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-400"></div>
                   <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
                 </label>
@@ -230,6 +230,7 @@ const editEquipementConfiguration = () => {
 }
 
 const saveEquipement = async () => {
+  console.log(equipement.value)
   const equipementTemp = {
     typeEquipement: '/api/type_equipements/' + equipement_selected.value,
     fitArena: '/api/fit_arenas/' + props.id,
