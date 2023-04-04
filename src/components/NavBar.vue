5a0102c8b41b3b013a91a3cba295525fdce0a040
rmingon <rmingon@doing.fr>
2023-04-04 15:51:08 +0200

    add scroll

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="fixed w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    <nav class="fixed w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700 z-10">
       <div class="h-14 flex items-center">
         <div class="flex justify-between w-10/12 px-4">
           <breadcrumbs :items="route.meta.breadcrumbs" />
