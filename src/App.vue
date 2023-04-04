a4a12700ceccae555c7f8a718e60ead58325ad21
rmingon <rmingon@doing.fr>
2023-04-04 15:52:50 +0200

    add scroll

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <SideNav  class="min-w-1/4"></SideNav>
     <div class=" w-full h-full bg-gray-50 lg:ml-64 dark:bg-gray-900">
       <NavBar/>
-      <div class="w-full pt-20 px-4 bg-gray-100 overflow-auto">
+      <div class="w-full pt-20 px-4 bg-gray-100 overflow-auto min-h-screen">
         <router-view></router-view>
       </div>
     </div>
