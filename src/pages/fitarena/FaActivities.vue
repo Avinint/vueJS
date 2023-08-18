110a6e527eace69ab88e9f6143b8eee9ec8eb300
Bruno <bavinint@doing.fr>
2023-08-18 15:57:08 +0200

    FITE2D--2937 : Suppression du caractère obligatoire du champ description dans formulaire activites

--- src/pages/fitarena/FaActivities.vue
+++ src/pages/fitarena/FaActivities.vue
@@ -132,7 +132,6 @@
           :readonly="readonly"
           type="text"
           class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-          required
         ></textarea>
       </div>
       <div class="flex items-center">
