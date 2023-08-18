ad9a58531afd22019f8ef18343d5354d76b9c878
Bruno <bavinint@doing.fr>
2023-08-18 10:20:55 +0200

    FITE2D-2573 : Ajout composant mention champs obligatoires

---
+++ src/components/common/MentionChampsObligatoires.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const props = defineProps({
+    marginTop: {type: String, default: '10px'}
+  })
+</script>
+
+<template>
+<p :style="{ marginTop: props.marginTop}">* Champ obligatoire</p>
+</template>
+
+<style scoped lang="scss">
+
+
+</style>
