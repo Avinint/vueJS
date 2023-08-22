a63c1c1a6e3bec4c5284dba4e7700f9503e2a571
Bruno <bavinint@doing.fr>
2023-08-22 15:41:02 +0200

    FITE2D-2573 : correctif css sur la page sousEspaces

--- src/pages/fitarena/FaSubSpaces.vue
+++ src/pages/fitarena/FaSubSpaces.vue
@@ -144,7 +144,7 @@
             class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300"
           ></div>
           <span
-            class="ml-3 text-sm font-medium text-gray-9000"
+            class="ml-3 text-sm font-medium text-gray-900"
           ></span>
         </label>
       </div>
