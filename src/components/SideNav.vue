da912476e12a5891ef6173a23e6ca10aec3e58f1
jbianchin <jbianchin@doing.fr>
2023-09-15 16:23:21 +0200

    ajout page résa pour les gestCo

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -160,15 +160,13 @@ const fitArenaLinks = computed(() => useMenuStore().fitArenas.length ? [
             path: `/fitarena/${fa.id}/planning`
           },
           {
+            label: 'Liste des réservations',
+            path: `/fitarena/${fa.id}/reservations`
+          },
+          {
             label: 'Supervision de la Fit Arena',
             path: `/fitarena/${fa.id}/supervision`
           },
-
-          // {
-          //   label: 'Liste des réservations',
-          //   path: `/fitarena/${fa.id}/reservations`,
-          //   tag: 1
-          // },
           // {
           //   label: 'Statistiques',
           //   path: `/fitarena/${fa.id}/statistiques`,
