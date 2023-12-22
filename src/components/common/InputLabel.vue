1202df65cf0062a1bd564e48352bb6ee12023a43
jbianchin <jbianchin@doing.fr>
2023-12-22 14:52:10 +0100

    fix inputLabel

--- src/components/common/InputLabel.vue
+++ src/components/common/InputLabel.vue
@@ -2,7 +2,7 @@
   <label
     :for="props.id"
     :class="{required: props.required}"
-    class="mb-2 block text-sm font-medium text-gray-900"
+    class="mb-2 block w-1/2 text-sm font-medium text-gray-900"
     ><slot></slot
   ></label>
 </template>
