0dbcfa136973d4e13159f64bd2d834b76c0ab2c6
jbianchin <jbianchin@doing.fr>
2023-12-22 14:43:13 +0100

    fix composant Table

--- src/components/common/Table.vue
+++ src/components/common/Table.vue
@@ -40,11 +40,11 @@
               <slot :name="`col-${key}`" :item="item.data" :index="index" />
             </td>
           </template>
-          <td>
+          <!-- <td v-if="statut">
             <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
               <circle cx="5" cy="5" r="5" :fill="item.data.statut.includes('acceptée') ? '#36A300' : '#D00000' " />
             </svg>
-          </td>
+          </td> -->
           <td v-if="editable && item.editable">
             <Button
               test="TeditElement"
