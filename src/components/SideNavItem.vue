eda493a8aac97a6ec4a67adb7e02e651208835d2
jbianchin <jbianchin@doing.fr>
2024-02-15 15:18:38 +0100

    FITE2D-3540 nom organisme

--- src/components/SideNavItem.vue
+++ src/components/SideNavItem.vue
@@ -4,7 +4,7 @@
     class="flex w-full items-center justify-between"
   >
     <div class="text-red-600"><slot></slot></div>
-    <span class="flex-1 whitespace-nowrap ml-3">{{ label }}</span>
+    <span class="flex-1 ml-3">{{ label }}</span>
     <span
       v-if="tag"
       class="ml-3 inline-flex items-center justify-center rounded-full w-5 px-1 text-sm font-medium bg-red-500 text-white"
